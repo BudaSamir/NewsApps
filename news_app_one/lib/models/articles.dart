a93d5d6685a1ee63f125e229be5ff119b66f4256
Buda Samir <Abdulrahman.samir.attia.elseed.96@gmail.com>
2023-07-11 22:39:10 +0300

    created 2 Models (article + articles)

---
+++ news_app_one/lib/models/articles.dart
@@ -0,0 +1,7 @@
+class Articles {
+  final List<dynamic> articles;
+  Articles({required this.articles});
+  factory Articles.fromJson(Map<String, dynamic> jsonData) {
+    return Articles(articles: jsonData['articles']);
+  }
+}
